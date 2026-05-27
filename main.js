825230ded4eeb5b164771bc4dd8d6946fdc07dc9
ggyu1213 <ggyu1213@gmail.com>
2026-05-27 04:27:44 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 const generateButton = document.getElementById('generate');
 const numberElements = document.querySelectorAll('.number');
+const themeSwitch = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        themeSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+themeSwitch.addEventListener('change', switchTheme, false);
 
 function generateNumbers() {
     const numbers = new Set();
